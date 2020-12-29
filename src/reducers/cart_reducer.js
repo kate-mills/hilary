8fc221469e4d7104074ffe6b32d81c39861add54
Kate Mills <kate.mills1124@gmail.com>
2020-12-29 15:52:24 -0800

    Add item to cart

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -11,23 +11,36 @@ const cart_reducer = (state, action) => {
   if(action.type == ADD_TO_CART){
     const {id, color, quantity, item} = action.payload
     const tempItem = state.cart.find((i) => i.id === id+color)
+
     if(tempItem){
-      console.log('already in cart.')
+      const tempCart = state.cart.map((cartItem) =>{
+        // Double check id 
+        if(cartItem.id === id + color){
+          // Check stock quantity
+          let newQuantity = cartItem.quantity + quantity
+          if(newQuantity > cartItem.max) {
+            newQuantity = cartItem.max 
+          }
+          // End Check stock quantity
+          return {...cartItem, quantity: newQuantity}
+        } else{
+          // cart id does not match id + color
+          return cartItem
+        }
+      })
       }else{
-        console.log('item in reducer', item)
-      const newItem = {
-        id: id+color,
-        name:item.name,
-        color,
-        quantity,
-        image:item.images[0].fluid,
-        wholePrice: item.wholePrice,
-        price:item.price,
-        max:item.stockQuantity
-      }
-        console.log('newItem', newItem);
-      return {...state, cart:[...state.cart, newItem]}
+        const newCartItem = {
+          id: id+color,
+          name:item.name,
+          color,
+          quantity,
+          image:item.images[0].fluid,
+          wholePrice: item.wholePrice,
+          price:item.price,
+          max:item.stockQuantity, }
+      return {...state, cart:[...state.cart, newCartItem]}
     }
+    return {...state}
   }
 
   throw new Error(`No Matching "${action.type}" - action type`)
