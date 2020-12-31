b26d267eb316b20828f92be7eabc4203887520d6
Kate Mills <kate.mills1124@gmail.com>
2020-12-30 16:40:01 -0800

    Added back featured_items query

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -32,18 +32,38 @@ const query = graphql`
         }
       }
     }
+    featuredItems:allContentfulHilaryJewelry(filter: {featured: {eq: true}}, sort: {order: ASC, fields: name}) {
+      edges {
+        node {
+          colors
+          category
+          description { description }
+          featured
+          id
+          images { 
+            fluid(resizingBehavior: SCALE){
+              ...GatsbyContentfulFluid
+            }
+          }
+          name
+          onSale
+          price
+          reviewCount
+          slug
+          stars
+          stockQuantity
+          wholePrice
+        }
+      }
+    }
   }
 `
-
-const initialState = { isSidebarOpen: false, products_loading: false, products_error: false, all_items: [] };
-
+const initialState = { isSidebarOpen: false, products_loading: false, products_error: false, all_items: [], featured_items: [] };
 const ProductsContext = React.createContext();
 
 export const ProductsProvider = ({ children }) => {
-  const { allItems } = useStaticQuery(query);
-
+  const { allItems, featuredItems } = useStaticQuery(query);
   const [state, dispatch] = useReducer(reducer, initialState);
-
   const openSidebar =  () => { dispatch({ type: SIDEBAR_OPEN }) };
   const closeSidebar = () => { dispatch({ type: SIDEBAR_CLOSE }) };
 
@@ -54,13 +74,11 @@ export const ProductsProvider = ({ children }) => {
         openSidebar,
         closeSidebar,
         all_items: allItems.edges,
-      }}
-    >
+        featured_items: featuredItems.edges,
+      }}>
       {children}
     </ProductsContext.Provider>
   )
 };
 
-export const useProductsContext = () => {
-  return useContext(ProductsContext)
-};
+export const useProductsContext=()=>{return useContext(ProductsContext)};
